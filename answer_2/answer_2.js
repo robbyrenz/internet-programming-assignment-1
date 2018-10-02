fe4802abe3e9277a3474a06ccc8c09322857b0cf
Robby Renz <nxgrenz@gmail.com>
2018-10-02 18:08:47 -0500

    made a class in the .js file, which is not working

--- answer_2/answer_2.js
+++ answer_2/answer_2.js
@@ -1,45 +1,81 @@
-window.onload = init;
+let placeholder = new ParagraphChanger(document.getElementById("change"));
+// window.onload = new ParagraphChanger(document.getElementById("change"));
+window.onload = placeholder.bold;
 
-function init() {
-    document.getElementById("bold").onclick = function() {
-        let foo = document.getElementById("change");
+class ParagraphChanger {
+    constructor(paragraph) {
+        this.paragraph = paragraph;
+        for (let i = 0; i < 4; i++) {
+            document.createElement("button");
+            document.getElementsByTagName("Button")[i].addEventListener('click', (i) => {
+                switch (i) {
+                    case 0: this.bold(); console.log("Hello World"); break;
+                    case 1: // function; break;
+                    case 2: // function; break;
+                    case 3: // function; break;
+                }
+            });
+        }
+    };
+    bold() {
+        let foo = this.paragraph;
         if (foo.style.fontWeight === "bold") {
             foo.style.fontWeight = "normal";
         } else {
             foo.style.fontWeight = "bold";
         }
+    };
+    position() {
+        // more code here
     }
-
-    document.getElementById("position").onclick = function() {
-        let foo = document.getElementById("change");
-        if (foo.style.position === 'static') {
-            foo.style.position = 'relative';
-            foo.style.top = '200px';
-        } else {
-            foo.style.position = 'static';
-        }
+    color() {
+        // some more code here
     }
-
-    document.getElementById("color").onclick = function() {
-        let foo = document.getElementById("change");
-        if (foo.style.color === 'black') {
-            foo.style.color = "#c5c5c5";
-            foo.style.backgroundColor = "#33ff3f";
-        } else {
-            foo.style.color = 'black';
-            foo.style.backgroundColor = 'white';
-        }
-    }
-
-    document.getElementById("size").onclick = function() {
-        let foo = document.getElementById("change");
-        if (foo.style.fontSize === '1.5em') {
-            foo.style.fontSize = "2em";
-        } else {
-            foo.style.fontSize = '1.5em';
-        }
+    size() {
+        // the last piece of code here
     }
 }
+
+// function init() {
+//     document.getElementById("bold").onclick = function() {
+//         let foo = document.getElementById("change");
+//         if (foo.style.fontWeight === "bold") {
+//             foo.style.fontWeight = "normal";
+//         } else {
+//             foo.style.fontWeight = "bold";
+//         }
+//     }
+
+//     document.getElementById("position").onclick = function() {
+//         let foo = document.getElementById("change");
+//         if (foo.style.position === 'static') {
+//             foo.style.position = 'relative';
+//             foo.style.top = '200px';
+//         } else {
+//             foo.style.position = 'static';
+//         }
+//     }
+
+//     document.getElementById("color").onclick = function() {
+//         let foo = document.getElementById("change");
+//         if (foo.style.color === 'black') {
+//             foo.style.color = "#c5c5c5";
+//             foo.style.backgroundColor = "#33ff3f";
+//         } else {
+//             foo.style.color = 'black';
+//             foo.style.backgroundColor = 'white';
+//         }
+//     }
+
+//     document.getElementById("size").onclick = function() {
+//         let foo = document.getElementById("change");
+//         if (foo.style.fontSize === '1.5em') {
+//             foo.style.fontSize = "2em";
+//         } else {
+//             foo.style.fontSize = '1.5em';
+//         }
+//     }
+// }
 
 // function init() {
 //     let buttons = document.getElementsByTagName("button");
